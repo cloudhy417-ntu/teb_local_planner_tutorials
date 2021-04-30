cdd69c58e4e6362060a42012e4a13d4d205a64ff
cloudhy417-ntu <r08921013@ntu.edu.tw>
2021-04-30 23:07:39 +0800

    human group layer

--- scripts/MPDM.py
+++ scripts/MPDM.py
@@ -6,14 +6,19 @@ from sklearn.cluster import DBSCAN
 from nav_msgs.srv import GetPlan
 from nav_msgs.msg import Path
 from std_msgs.msg import Header, ColorRGBA
-from geometry_msgs.msg import Vector3, Point32
+from geometry_msgs.msg import Vector3, Point32, PolygonStamped
 from costmap_converter.msg import ObstacleArrayMsg, ObstacleMsg
+from jsk_recognition_msgs.msg import PolygonArray
 import sys
 print (sys.version)
-sys.path.insert(0, '/home/chy/python3_ws/devel/lib/python3/dist-packages')
+sys.path.insert(0, '/home/cloudhy/python3_ws/devel/lib/python3/dist-packages')
 from tf.transformations import quaternion_from_euler
 from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
 from visualization_msgs.msg import Marker, MarkerArray
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from std_srvs.srv import Empty
+
 PI = math.pi
 map_dict = {1:(0,-PI), 2:(PI/4, -3*PI/4), 3:(PI/2, -PI/2), 4:(3*PI/4, -PI/4)}
 class MPDM:
@@ -31,6 +36,7 @@ class MPDM:
         
         self.human_sub = rospy.Subscriber('/move_base/TebLocalPlannerROS/obstacles', ObstacleArrayMsg, self.humanObstacleCallback)
         self.cluster_visualizer = rospy.Publisher('/group_cluster', MarkerArray, queue_size = 1)
+        self.human_group_publisher = rospy.Publisher('/move_base/local_costmap/humangrouplayer/human_group', PolygonArray, queue_size=1)
         self.humans = ObstacleArrayMsg()
         self.robot_pose_sub = rospy.Subscriber('/robot_pose', Pose, self.update_pose)
         self.robot_pose = Pose()
@@ -55,10 +61,16 @@ class MPDM:
         humanArr = np.array(humanArr)
         clustering=DBSCAN(eps=1.2,min_samples=2).fit(humanArr)
         group_id = 0
+        group_polygons = PolygonArray()
+        group_polygons.header.frame_id = '/map'
         while group_id in clustering.labels_:
+            group_polygon = PolygonStamped()
+            group_polygon.header.frame_id = '/map'
             indexes = np.where(clustering.labels_ == group_id)
             group_positions = humanArr[indexes]
             position = np.average(group_positions, axis=0)
+            if(group_positions.shape[0]==2):
+                group_positions = np.concatenate((group_positions, np.expand_dims(position, 0)), axis=0)
             Xrange = group_positions[:,0].max() - group_positions[:,0].min() + 1
             Yrange = group_positions[:,1].max() - group_positions[:,1].min() + 1
             marker = Marker(
@@ -71,6 +83,10 @@ class MPDM:
             marker.lifetime = rospy.Duration.from_sec(0.5)
             group_id += 1
             markerArray.markers.append(marker)
+            for position in group_positions:
+                group_polygon.polygon.points.append(Point32(position[0], position[1], 0))
+            group_polygons.polygons.append(group_polygon)    
+        self.human_group_publisher.publish(group_polygons)
         self.cluster_visualizer.publish(markerArray)
     def update_pose(self, pose):
         self.robot_pose = pose
@@ -108,7 +124,7 @@ class MPDM:
         return total_score/(len(path.poses)-1)
 if __name__=='__main__':
     rospy.init_node('mpdm', anonymous=True)
-    flowmap = np.load('/home/chy/catkin_ws/src/teb_local_planner_tutorials/scripts/flowmap_direction_down_sampled.npy')
+    flowmap = np.load('/home/cloudhy/catkin_ws/src/teb_local_planner_tutorials/scripts/flowmap_direction_down_sampled.npy')
     resolution = 0.2
     centX = 32.5
     centY = 22.5
